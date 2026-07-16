e6a785733597287b2ffa5f7e5cf19c46ea19d530
Xaniyf <162134595+Xaniyf@users.noreply.github.com>
2026-07-16 13:04:37 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Bot muvaffaqiyatli ishga tushdi!"
+    }
+)
+
+print("Bot started.")

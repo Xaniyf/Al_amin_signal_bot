41caa5510423dc64ad678aca0d5f9dff35e01df5
Xaniyf <162134595+Xaniyf@users.noreply.github.com>
2026-07-16 14:31:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,25 @@
 import os
-import time
 import requests
+import time
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+def send_signal(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
-    url,
-    data={
+    requests.post(url, data={
         "chat_id": CHAT_ID,
-        "text": "✅ Bot Render'da muvaffaqiyatli ishga tushdi!"
-    }
-)
+        "text": text
+    })
 
-print("Bot started.")
+while True:
+    send_signal(
+        "🚀 Yangi signal!\n\n"
+        "Coin: TEST\n"
+        "Entry: 0.001$\n"
+        "Target: +50%\n"
+        "Risk: DYOR"
+    )
+
+    time.sleep(3600)  # har 1 soatda yuboradi
